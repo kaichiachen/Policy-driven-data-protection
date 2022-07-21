aad89cc27484b0a3e11f737aeb6645f0ff59f32a
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-07-21 12:11:13 +0800

    change directories to be local

--- dev.sh
+++ dev.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-mkdir -p ~/test/frontend
-mkdir -p ~/test/backend
-mkdir -p ~/test/objectserver1
-mkdir -p ~/test/objectserver2
-mkdir -p ~/test/objectserver3
-mkdir -p ~/test/objectserver4
-mkdir -p ~/test/objectserver5
-mkdir -p ~/test/objectserver6
+mkdir -p ./test/frontend
+mkdir -p ./test/backend
+mkdir -p ./test/objectserver1
+mkdir -p ./test/objectserver2
+mkdir -p ./test/objectserver3
+mkdir -p ./test/objectserver4
+mkdir -p ./test/objectserver5
+mkdir -p ./test/objectserver6
 
 OBJECTSERVER_KEY=12345 docker compose -f docker-compose.dev.yml up
